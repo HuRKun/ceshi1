00522cc4d75627b8132caeac71921bac82ae746e
胡荣坤 <hurongkun@hurongkundemacbook-pro.local>
2016-02-19 12:46:13 +0800

    Initial Commit

---
+++ RKWedding/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RKWedding
+//
+//  Created by 胡荣坤 on 16/2/19.
+//  Copyright © 2016年 HuRongKun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
